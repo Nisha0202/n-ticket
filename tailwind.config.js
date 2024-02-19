87e96e6c71d32f2d316bb0a9de5150bde67f4a62
Nisha0202 <nishajabatunnessa@gmail.com>
2024-02-19 18:45:28 +0600

    designing done

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: ["./src/**/*.{html,js}"],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
+
+//   module.exports = {
+//     theme: {
+//       colors: {
+//         'myColor': '#1DD100', // replace '#1da1f2' with your hex color
+//       },
+//     },
+//   }
+  
+  module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          'myColor': '#1DD100', // replace with your desired hex color
+        },
+      },
+    },
+    plugins: [],
+  };
+  
