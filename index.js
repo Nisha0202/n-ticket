e7db8654d48f64930399005ef4fa28741fc2430a
Nisha0202 <nishajabatunnessa@gmail.com>
2024-02-29 16:07:20 +0600

    functionality updated

--- index.js
+++ index.js
@@ -1,127 +1,28 @@
-
-let selected=0; //seat selected
-let totalSeat = 12; //total seats
+let selected = 0; //seat selected
+let totalSeat = 32; //total seats
 let totalprice = 0;
-
 const seats = document.querySelectorAll('.seat');
-const seatCount= document.querySelector('#seatCount');
-const booked= document.querySelector('#booked');
+const seatCount = document.querySelector('#seatCount');
+const booked = document.querySelector('#booked');
 const table = document.querySelector('.tabletwo')
-
 const price = document.querySelector('#price')
+const discountprice = document.querySelector('#discount-price');
+const input_coupon = document.querySelector('.coupon-inp-btn');
 
 
-seats.forEach(seat =>{
-    
-    seat.addEventListener('click',()=>{
-      selected++;
-      totalSeat--; // button counts decrease
-      booked.textContent =`${selected}`
-      seatCount.textContent = `${totalSeat} Seats left` //show seat left
-      disablePhonenumber();//enable
-
-      seat.style.backgroundColor="#1DD100"; //color change
-      seat.style.color = "#ffffff"
-      seat.disabled = 'true' //preventing clicking twice
-      if(selected == 4){  // seat clicked at most 4
-         seats.forEach(seat =>{
-            seat.disabled = 'true'
-         });
-      }
- 
-    //   list of taken seat
-      const newTr = document.createElement('tr');
-
-      newTr.innerHTML= `<td>${seat.id}</td>
-                          <td>Economy</td>
-                          <td>550</td>`;
-      table.appendChild(newTr);
-
-
-
-// count money
-
- totalprice = selected*550;
-price.textContent = `BDT ${totalprice}`
-
-    
-    } 
-    )
-
-
-
-})
-
-
-// form-fiiled or not
-
-const nameinp = document.querySelector('.name');
-const phone = document.querySelector('.phone');
-const email = document.querySelector('.email');
-const next = document.querySelector('.next');
-
-function checkInputs() {
-    if (nameinp.value && phone.value && email.value) {
-        if ((phone.value.length != 0) && (selected != 0) ) { // checking phone number and selected seat
-            next.removeAttribute('disabled');
-        }
-    };
+// seat color change
+function seatselected(seat) {
+    seat.style.backgroundColor = "#1DD100";
+    seat.style.color = "#ffffff";
+    seat.disabled = 'true';
 }
 
-nameinp.addEventListener('input', checkInputs);
-phone.addEventListener('input', checkInputs);
-email.addEventListener('input', checkInputs);
+// show left seat
+function left_selectedseat(selected, totalSeat) {
+    booked.textContent = `${selected}`
+    seatCount.textContent = `${totalSeat} Seats left` //show seat left
 
-
-
-// enable coupn button
-const coupn = document.querySelector('.coupon-inp');
-const button = document.querySelector('.coupon-inp-btn');
-const msg = document.querySelector('#thanku');
-
-function checkCoupnsinpgiven() {
-    if (coupn.value) {
-        button.removeAttribute('disabled')
-    }   
-    else{
-        button.setAttribute('disabled', 'true');
-    }
 }
-coupn.addEventListener('input', checkCoupnsinpgiven);
-checkCoupnsinpgiven();
-
-
-// coupn section hidden 
-const section = document.querySelector('.input-secttioncpn');
-const discountprice =document.querySelector('#discount-price');
-let discount = 0;
-
-button.addEventListener('click', () => {
-    console.log(coupn.value);
-    if (coupn.value == 'NEW15' || coupn.value == 'Couple 20') {
-        
-        section.setAttribute('hidden', 'true');
-        msg.removeAttribute('hidden');
-
-        if(coupn.value == 'NEW15'){
-            discount = totalprice - totalprice*0.15;
-            discountprice.textContent = `BDT ${discount}`
-        }
-        else{
-            discount = totalprice - totalprice*0.20;
-            discountprice.textContent = `BDT ${discount}`
-        }
-
-    }else{
-        alert("Wrong Coupon");    }
-});
-
-// reload window 
-next.addEventListener('click', function() {
-    setTimeout(function() {
-        location.reload();
-    }, 4000);
-});
 
 
 // disable phon number field
@@ -132,4 +33,125 @@ function disablePhonenumber() {
         phone.removeAttribute('disabled');
     }
 }
+
+// taken seat view
+function takenseatadded(seat) {
+    //   list of taken seat
+    const newTr = document.createElement('tr');
+
+    newTr.innerHTML = `<td>${seat.id}</td>
+                      <td>Economy</td>
+                      <td>550</td>`;
+    table.appendChild(newTr);
+
+}
+
+// total prices and discount price update
+function calPrice() {
+    // count money
+
+    totalprice = selected * 550;
+    price.textContent = `BDT ${totalprice}`;
+    discountprice.textContent = `BDT ${totalprice}`;
+
+}
+
+//disable copon
+function disabledcoupon() {
+    if (selected === 0) {
+        input_coupon.setAttribute('disabled', 'true');
+    } else {
+        input_coupon.removeAttribute('disabled');
+    }
+};
+
+
+
+// clicking on seat button
+seats.forEach(seat => {
+
+    seat.addEventListener('click', () => {
+
+        selected++;
+        totalSeat--;
+
+        seatselected(seat);
+        left_selectedseat(selected, totalSeat);
+        disablePhonenumber(selected);
+        takenseatadded(seat);
+        calPrice(selected);
+        disabledcoupon(selected);
+
+        //select atmost 4
+        if (selected == 4) {
+            seats.forEach(seat => {
+                seat.disabled = 'true';
+            })
+        }
+    })
+
+})
+
+
+// enable coupn button
+const coupn = document.querySelector('.coupon-inp');
+const button = document.querySelector('.coupon-inp-btn');
+const msg = document.querySelector('#thanku');
+const msgalert = document.querySelector('#wrong');
+
+
+// coupn section hidden 
+const section = document.querySelector('.input-secttioncpn');
+let discount = 0;
+
+function applybutton(){
+    if (coupn.value == 'NEW15' || coupn.value == 'Couple 20') {
+
+        section.setAttribute('hidden', 'true');
+        msg.removeAttribute('hidden');
+
+        if (coupn.value == 'NEW15') {
+            discount = totalprice - totalprice * 0.15;
+            discountprice.textContent = `BDT ${discount}`
+        }
+        else {
+            discount = totalprice - totalprice * 0.20;
+            discountprice.textContent = `BDT ${discount}`
+        }
+    } 
+ else {
+        msgalert.removeAttribute('hidden');
+    }
+}
+
+button.addEventListener('click', () => {
+    applybutton();
+});
+
+
+
+// form-fiiled or next button disable
+const nameinp = document.querySelector('.name');
+const phone = document.querySelector('.phone');
+const email = document.querySelector('.email');
+const next = document.querySelector('.next');
+
+function checkInputs() {
+    if (nameinp.value && phone.value && email.value) {
+            next.removeAttribute('disabled');
+    };
+}
+
+nameinp.addEventListener('input', checkInputs);
+phone.addEventListener('input', checkInputs);
+email.addEventListener('input', checkInputs);
+
+// reload window 
+next.addEventListener('click', function () {
+    setTimeout(function () {
+        location.reload();
+    }, 4000);
+});
+
+
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,10 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//     content: ["./src/**/*.{html,js}"],
-//     theme: {
-//       extend: {},
-//     },
-//     plugins: [],
-//   }
-
-//   module.exports = {
-//     theme: {
-//       colors: {
-//         'myColor': '#1DD100', // replace '#1da1f2' with your hex color
-//       },
-//     },
-//   }
-  
-  module.exports = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {
-        colors: {
-          'myColor': '#1DD100', // replace with your desired hex color
-        },
-      },
+      extend: {},
     },
     plugins: [],
   };
+
   
